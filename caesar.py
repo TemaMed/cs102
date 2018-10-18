7a9ede58e5e11de088f561e7859b83742c7c2def
TemaMed <43575329+TemaMed@users.noreply.github.com>
2018-10-18 22:36:35 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,20 +1,43 @@
-def encrypt_caesar(plaintext):
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  ciphertext = ""
-  for i in range(len(plaintext)):
-     up = False
-     if plaintext[i].isupper():
+def encrypt_caesar(plaintext:str)->str:
+    """
+        >>> encrypt_caesar("PYTHON")
+        'SBWKRQ'
+        >>> encrypt_caesar("python")
+        'sbwkrq'
+        >>> encrypt_caesar("Python3.6")
+        'Sbwkrq3.6'
+        >>> encrypt_caesar("")
+        ''
+        """
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    ciphertext = ""
+    for i in range(len(plaintext)):
+      up = False
+      if plaintext[i].isupper():
          up = True
-     if plaintext[i].lower() in alphabet:
+      if plaintext[i].lower() in alphabet:
         if up:
          ciphertext += alphabet[(3 + alphabet.index(plaintext[i].lower())) % len(alphabet)].upper()
         else:
          ciphertext += alphabet[(3 + alphabet.index(plaintext[i])) % len(alphabet)]
-  return ciphertext
-def encrypt_decaesar(ciphertext):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    plaintext = ""
-    for i in range(len(ciphertext)):
+      else:
+         ciphertext += plaintext[i]
+    return ciphertext
+
+def decrypt_caesar(ciphertext):
+     """
+        >>> decrypt_caesar("SBWKRQ")
+        'PYTHON'
+        >>> decrypt_caesar("sbwkrq")
+        'python'
+        >>> decrypt_caesar("Sbwkrq3.6")
+        'Python3.6'
+        >>> decrypt_caesar("")
+        ''
+         """
+     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+     plaintext = ""
+     for i in range(len(ciphertext)):
         up = False
         if ciphertext[i].isupper():
          up = True
@@ -25,4 +48,4 @@ def encrypt_decaesar(ciphertext):
                 plaintext += alphabet[((alphabet.index(ciphertext[i].lower()))-3) % len(alphabet)]
         else:
             plaintext += ciphertext[i]
-    return plaintext
+     return plaintext
