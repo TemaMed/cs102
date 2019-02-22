1a24b081ef1d59da98774a4d33eccd24dca4667e
TemaMed <43575329+TemaMed@users.noreply.github.com>
2019-02-22 22:44:42 +0300

    Add files via upload

---
+++ homework06/hackernews.py
@@ -0,0 +1,91 @@
+from bottle import (
+    route, run, template, request, redirect
+)
+import bottle
+import string
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+from scraputils import get_news
+from db import News, session
+from bayes import NaiveBayesClassifier
+
+
+@route("/news")
+def news_list():
+    s = session()
+    rows = s.query(News).filter(News.label == None).all()
+    return template("news_template", rows=rows)
+
+
+@route("/add_label/")
+def add_label():
+    s = session()
+    news = s.query(News).filter(News.id == request.query.id).one()
+    news.label = request.query.label
+    s.commit()
+    redirect("/news")
+
+
+@route("/update")
+def update_news():
+    s = session()
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages=5)
+    news_list_bd = s.query(News).all()
+    if len(news_list_bd) > 0:
+        for new_news in news_list:
+            f = False
+            for old_news_bd in news_list_bd:
+                if new_news['author'] == old_news_bd.author and new_news['title'] == old_news_bd.title:
+                    f = True
+                    break
+            if not f:
+                s.add(News(**new_news))
+
+    else:
+        for new_news in news_list:
+            s.add(News(**new_news))
+    s.commit()
+    redirect("/news")
+
+
+@route("/classify")
+def classify_news():
+    s = session()
+    labeled_news = s.query(News).filter(News.label != None).all()
+    x_train = [clean(news.title) for news in labeled_news]
+    y_train = [news.label for news in labeled_news]
+    classifier.fit(x_train, y_train)
+
+    rows = s.query(News).filter(News.label == None).all()
+    good, maybe, never = [], [], []
+    for row in rows:
+        [prediction] = classifier.predict([clean(row.title)])
+        if prediction == 'good':
+            good.append(row)
+        elif prediction == 'maybe':
+            maybe.append(row)
+        else:
+            never.append(row)
+
+    return template('news_information', good=good, maybe=maybe, never=never)
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator).lower()
+
+
+if __name__ == "__main__":
+    abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
+    abs_views_path = os.path.join(abs_app_dir_path, 'templates')
+    bottle.TEMPLATE_PATH.insert(0, abs_views_path)
+    s = session()
+    labeled_news = s.query(News).all()
+    x_train = [clean(news.title) for news in labeled_news]
+    y_train = [news.label for news in labeled_news]
+    classifier = NaiveBayesClassifier()
+    classifier.fit(x_train, y_train)
+
+    run(host="localhost", port=8080)
---
+++ homework06/scraputils.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import db
+
+
+def extract_news(parser):
+    """ Extract news from a given web page """
+    news_list = []
+    news_table = parser.find_all("tr", {"class": "athing"})
+    info = parser.find_all("td", {"class": "subtext"})
+    for i in range(len(news_table)):
+        comments = "0"
+        links = info[i].find_all("a")
+        title = news_table[i].find(class_="storylink")
+        points = info[i].span.text.split()[0]
+        try:
+            if links[3].text != "discuss":
+                comments = links[3].text.split()[0]
+        except Exception as ex:
+            pass
+
+        news = {
+            'author': links[0].text,
+            'comments': comments,
+            'points': points,
+            'title': title.text,
+            'url': title['href'],
+            'label': None
+        }
+        news_list.append(news)
+
+    return news_list
+
+
+def extract_next_page(parser):
+    """ Extract next page URL """
+    return parser.table.find_all('table')[1].find_all('a')[-1]['href']
+    pass
+
+
+def get_news(url, n_pages=1):
+    """ Collect news from a given web page """
+    news = []
+    while n_pages:
+        print("Collecting data from page: {}".format(url))
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html5lib")
+        news_list = extract_news(soup)
+        next_page = extract_next_page(soup)
+        url = "https://news.ycombinator.com/" + next_page
+        news.extend(news_list)
+        n_pages -= 1
+    return news
+
+
+news = get_news("https://news.ycombinator.com/", 1)
+"""for i in news:
+    print(i)
+s = db.session()
+for i in range(len(news)):
+    obj = db.News(title=news[i]['title'],
+    obj = db.News(title=news[i]['title'],
+                  author=news[i]['author'],
+                  url=news[i]['url'],
+                  comments=news[i]['comments'],
+                  points=news[i]['points'])
+    s.add(obj)
+s.commit()
+"""
+
